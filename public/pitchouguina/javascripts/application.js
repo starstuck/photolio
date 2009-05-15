f76906a416a9ccd9bdc9c971afbc8b9f58eb6344
Tomasz Szarstuk szarsti@syscenter.eu <szarsti@shamac.baza.scnet>
2009-05-15 12:32:48 +0200

    Minor compatybility updates

--- public/pitchouguina/javascripts/application.js
+++ public/pitchouguina/javascripts/application.js
@@ -8,7 +8,6 @@ Object.extend(Event, {
                 if (!event) event = window.event;
                 if (event.wheelDelta) {
                         delta = event.wheelDelta/120;
-                        if (window.opera) delta = -delta;
                 } else if (event.detail) { delta = -event.detail/3;     }
                 return Math.round(delta); //Safari Round
         }
