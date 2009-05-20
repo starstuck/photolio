6682dd994bb8345fd59d30d8ec6858f524aaf7aa
Tomasz Szarstuk <szarsti@xenbaza.baza.scnet>
2009-05-20 13:05:03 +0200

    Add topic macros tests

---
+++ test/unit/helpers/application_helper_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class DummyTopic; attr_accessor :body, :site; end
+
+class ApplicationHelperTest < ActionView::TestCase
+
+  def setup
+    super
+    @topic = DummyTopic.new
+    @topic.site = sites(:pitchouguina)
+  end
+
+  def test_unknown_topic_macro
+    @topic.body = 'Some [[my_macro]] text.'    
+    assert_equal 'Some  text.', render_topic(@topic)
+  end
+
+  def test_asset_image_topic_macro
+    @topic.body = 'Some [[asset_image_path(smart.jpg)]] asset.'
+    assert_equal 'Some /pitchouguina/files/assets/smart.jpg asset.', render_topic(@topic)
+    # Existing file
+    @topic.body = 'Some [[ asset_image_path ( love_stories.jpg ) ]] asset.'
+    assert_equal 'Some /pitchouguina/files/assets/love_stories.jpg asset.', render_topic(@topic)
+  end
+
+  def test_photo_image_topic_macro
+    @topic.body = 'Some [[photo_image_path(smart.jpg)]] asset.'
+    assert_equal 'Some /pitchouguina/files/photos/smart.jpg asset.', render_topic(@topic)
+  end
+
+end
